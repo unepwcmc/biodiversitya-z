77cfa55da33460fd7ba147243dad84262425fcca
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-05-13 10:34:01 +0100

    Added production deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,34 @@ set :ssh_options, {
 }
 
 
+namespace :deploy do
+
+  desc 'Restart application'
+  task :start do ; end
+  task :stop do ; end
+  task :restart do
+    on roles(:app), in: :sequence do
+      execute "touch #{File.join(current_path,'tmp','restart.txt')}"
+    end
+  end
+  after :publishing, :restart
+
+  desc 'Rebuild the sphinx config'
+  task :configure_sphinx do
+    on roles(:app), in: :sequence do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'ts:configure'
+        end
+      end
+    end
+  end
+  before :restart, :configure_sphinx
+end
+
+
+
+
 set :linked_files, %w{config/database.yml config/initializers/refinery/core.rb config/initializers/secret_token.rb}
 
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,26 @@
+set :stage, :production
+
+server "click-linode.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
+
+set :application, "biodiversitya-z"
+set :server_name, "biodiversitya-z.click-linode.linode.unep-wcmc.org"
+set :sudo_user, "wcmc"
+set :app_port, "80"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
